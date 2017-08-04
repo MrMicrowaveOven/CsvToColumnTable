8420230eaf52d88b4235460ae44b97071f8fe2d2
Benjamin <benj@zagorski.com>
2017-08-04 14:26:05 -0700

    Added basic view, properly calling controller, which is properly calling model, and responding all the way back to the view

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,9 +1,11 @@
 class TablesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def new
-
+    # Default will render `new.html`.
   end
 
   def create
-
+    table = Table.make_table(params[:csv], params[:numColumns])
+    render json: table
   end
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,2 +1,5 @@
 class Table < ApplicationRecord
+  def self.make_table(csv, num_columns)
+    csv.split(",")
+  end
 end
